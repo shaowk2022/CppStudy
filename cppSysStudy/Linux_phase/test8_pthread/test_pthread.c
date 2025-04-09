12bba6b2e31e66caa2bc6200c5abcafd4246dc1e
wenke <417496897@qq.com>
2025-04-09 17:36:00 +0800

    线程练习

--- cppSysStudy/Linux_phase/test8_pthread/test_pthread.c
+++ cppSysStudy/Linux_phase/test8_pthread/test_pthread.c
@@ -4,6 +4,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef struct {
+    int id;
+    char name[20];
+} Student;
+
 void print_ids(const char* prefix) {
     printf("%s: ", prefix);
     printf("pid = %d, ppid = %d, ", getpid(), getppid());
@@ -12,6 +17,13 @@ void print_ids(const char* prefix) {
 
 void* start_routine(void* args) {
     print_ids("new_thread");
+    // printf("args = %d\n", (int)args);
+
+    pthread_exit(NULL);  // 线程退出
+    printf("this msg will not be outputted.\n");
+
+    Student* stu = (Student*)args;
+    printf("id: %d, name: %s\n", stu->id, stu->name);  // 在子线程中访问主线程的数据stu
     return NULL;
 }
 
@@ -20,14 +32,15 @@ int main(int argc, char const* argv[]) {
 
     // 主线程
     print_ids("main");
-
+    Student stu = {1, "zhangsan"};
     pthread_t tid;
-    int err = pthread_create(&tid, NULL, start_routine, NULL);
+    // int err = pthread_create(&tid, NULL, start_routine, (void*)9527);
+    int err = pthread_create(&tid, NULL, start_routine, (void*)&stu);  // 非0为错误码
     if (err) {
         error(1, err, "pthread_create error.\n");
     }
 
-    printf("main: new_thread id = %lu\n", tid);
+    // printf("main: new_thread id = %lu\n", tid);
 
     pthread_join(tid, NULL);  // 子线程运行结束后再终止主线程
 
---
+++ cppSysStudy/Linux_phase/test8_pthread/test_pthread_join.c
@@ -0,0 +1,67 @@
+#include <error.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>  // malloc
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef struct {
+    int left;
+    int right;
+} Section;
+
+void print_ids(const char* prefix) {
+    printf("%s: ", prefix);
+    printf("pid = %d, ppid = %d, ", getpid(), getppid());
+    printf("tid = %lu\n", pthread_self());
+}
+
+void* start_routine(void* args) {
+    Section* section = (Section*)args;
+    int* sum = (int*)malloc(sizeof(int));  // 动态分配内存, 否则函数返回时result指向一个无效地址
+    if (sum == NULL) {
+        perror("malloc error");
+        pthread_exit(NULL);
+    }
+    *sum = 0;  // 初始化sum
+    for (size_t i = section->left; i <= section->right; i++) {
+        *sum += i;
+    }
+
+    return (void*)sum;
+    // pthread_exit((void*)sum);  // 线程结束时返回和
+}
+
+int main(int argc, char const* argv[]) {
+    // pthread_t id = pthread_self();
+
+    // 分别用两个线程对1-50求和、51-100求和
+    pthread_t tid1, tid2;
+    Section section1 = {1, 50};
+    Section section2 = {51, 100};
+
+    int err = pthread_create(&tid1, NULL, start_routine, &section1);  // 非0为错误码
+    if (err) {
+        error(1, err, "pthread_create error.\n");
+    }
+
+    err = pthread_create(&tid2, NULL, start_routine, &section2);
+    if (err) {
+        error(1, err, "pthread_create error.\n");
+    }
+    // 主线程等待返回结果并计算出1-100的和
+    int *result1, *result2;
+    err = pthread_join(tid1, (void**)&result1);  // join()会无限期等待，直到子线程结束
+    if (err) {
+        error(1, err, "pthread_join error, tid: %lu.\n", tid1);
+    }
+    err = pthread_join(tid2, (void**)&result2);
+    if (err) {
+        error(1, err, "pthread_join error, tid: %lu.\n", tid2);
+    }
+    printf("sum = %d + %d = %d\n", *result1, *result2, *result1 + *result2);
+
+    free(result1);
+    free(result2);  // 释放动态分配的内存
+    return 0;
+}
